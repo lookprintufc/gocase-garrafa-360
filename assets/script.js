ea6c897b776faaaf9581d26eea1b7400bcc4adcf
Patrick Magalhaes <patrickcmagal@gmail.com>
2023-09-21 16:09:31 -0300

    modelo final com animacao

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 const modelViewerColor = document.querySelector("model-viewer#bottle");
+modelViewerColor.src = 'garrafa_gocase_950ml.glb'
 const createAndApplyTexture = async (channel, event) => {
   let texture = await modelViewerColor.createTexture(event);
-  material = modelViewerColor.model.materials[3];
+  material = modelViewerColor.model.materials.find((material) => material.name === 'Estampa');
   material.pbrMetallicRoughness[channel].setTexture(texture);
 };
 
+function bottleSelected(glb) {
+  modelViewerColor.src = glb
+}
+
 modelViewerColor.addEventListener("load", () => {
+  modelViewerColor.animationName = 'abrirTampa'
+
   createAndApplyTexture(
     "baseColorTexture",
     "estampas-compress/mundo_magico-min.png"
@@ -14,12 +21,31 @@ modelViewerColor.addEventListener("load", () => {
   document
     .getElementById("colorPicker")
     .addEventListener("input", function (event) {
-      const colorCircle = document.getElementById("colorCircle");
       colorSelected(event.target.value);
     });
 });
 
+modelViewerColor.addEventListener("camera-change", () => {
+  let maxTime = 1320
+  let phi = modelViewerColor.getCameraOrbit().phi
+  let time = 0;
+
+  const phiMax = 1.1;
+  let proportion = 1 - phi / phiMax;
+  time = proportion * maxTime;
+  time = Math.round(time) / 1000;
+  if(time > 0)
+    modelViewerColor.currentTime =  time
+});
+
+modelViewerColor.addEventListener("finished", () => {
+ console.log("modelViewerColor finished")
+});
+
 function selectMaterials(names = []) {
+  if (!modelViewerColor?.model?.materials)
+    return []
+
   const materials = modelViewerColor.model.materials;
   const selectedMaterials = [];
   for (const name of names) {
@@ -40,7 +66,9 @@ function bottleCapMaterial() {
     "MaterialTampaBaixo",
     "MaterialTampaCima",
     "MaterialAlca",
-    "MaterialBaseTampaCima"
+    "MaterialBaseTampaCima",
+    "Metal_Pino",
+    "MaterialBaseTampaCima.001"
   ]);
 }
 
